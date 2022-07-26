888537de4e6ae4617624f02568d94045d86c31f5
theo <theodeloose@gmail.com>
2022-07-26 14:16:35 +0200

    Finised volume_battery script

--- volume_battery.c
+++ volume_battery.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <alsa/asoundlib.h>
+#include <alsa/mixer.h>
+#include <math.h>
 
 int main(int argc, char const *argv[])
 {
@@ -38,6 +41,27 @@ int main(int argc, char const *argv[])
 	}
 	fclose(fp);
 
+	long min, max;
+	long volume;
+	snd_mixer_t *handle;
+	snd_mixer_selem_id_t *sid;
+	const char *card = "default";
+	const char *selem_name = "Master";
+	snd_mixer_open(&handle, 0);
+	snd_mixer_attach(handle, card);
+	snd_mixer_selem_register(handle, NULL, NULL);
+	snd_mixer_load(handle);
+
+	snd_mixer_selem_id_alloca(&sid);
+	snd_mixer_selem_id_set_index(sid, 0);
+	snd_mixer_selem_id_set_name(sid, selem_name);
+	snd_mixer_elem_t *elem = snd_mixer_find_selem(handle, sid);
+
+	snd_mixer_selem_get_playback_volume(elem, SND_MIXER_SCHN_MONO, &volume);
+	snd_mixer_selem_get_playback_volume_range(elem, &min, &max);
+
+	float soundPercent = roundf((float)(long)volume / (float)max * 100.00);
+
 	if (intBattery <= 20)
 		strcpy(batteryIcon, "\0");
 
@@ -54,7 +78,7 @@ int main(int argc, char const *argv[])
 		strcpy(batteryIcon, "\0");
 
 	if (strcmp(batteryStatus, "Discharging") == 0)
-		strcpy(batteryStatusIcon, "\0");
+		strcpy(batteryStatusIcon, " \0");
 
-	printf("%s %d %s\n", batteryStatusIcon, intBattery, batteryIcon);
+	printf("%.0f 墳 %s%d %s\n", soundPercent, batteryStatusIcon, intBattery, batteryIcon);
 }
